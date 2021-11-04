c3ae0771aaa1bca71c9fd7ef1d0a80a50818f010
Mdigennaro <michele.digennaro719@gmail.com>
2021-11-04 19:03:10 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -14,33 +14,53 @@ const utenti = [
     ruolo : 'Founder & CEO'
   },
   {
-    foto : 'img/an.jpg',
-    nome : 'Wayne Barnett',
-    ruolo : 'Founder & CEO'
+    foto : 'img/angela-caroll-chief-editor.jpg',
+    nome : 'Angela Caroll',
+    ruolo : 'Chief Editor'
   },
+  {
+    foto : 'img/walter-gordon-office-manager.jpg',
+    nome : 'Walter Gordon',
+    ruolo : 'Office Manager'
+  },
+  {
+    foto : 'img/angela-lopez-social-media-manager.jpg',
+    nome : 'Angela Lopez',
+    ruolo : 'Social Media Manager'
+  },
+  {
+    foto : 'img/scott-estrada-developer.jpg',
+    nome : 'Scott Estrada',
+    ruolo : 'Developer'
+  },
+  {
+    foto : 'img/barbara-ramos-graphic-designer.jpg',
+    nome : 'Barbara Ramos',
+    ruolo : 'Graphic Designer'
+  }
 ];
 console.log(utenti);
 
-
-//prendo il container dove inserire le carte
-const facciataUtenti = document.querySelector('.team-container');
-console.log('facciata',facciataUtenti);
-
-const teamCard = document.createElement('div');
-teamCard.classList.add('team-card');
-facciataUtenti.append(teamCard);
-let cardImage = document.createElement('div');
-cardImage.classList.add("card-image");
-teamCard.append(cardImage);
-let cardText = document.createElement('div');
-cardText.classList.add("card-text");
-teamCard.append(cardText);
 
 
 //creare la team card da mettere nel container
 for (let i = 0; i < utenti.length; i++) {
   const utente = utenti[i];
+  //prendo il container dove inserire le carte
+  const facciataUtenti = document.querySelector('.team-container');
+  console.log('facciata',facciataUtenti);
   
+  const teamCard = document.createElement('div');
+  teamCard.classList.add('team-card');
+  facciataUtenti.append(teamCard);
+  let cardImage = document.createElement('div');
+  cardImage.classList.add("card-image");
+  teamCard.append(cardImage);
+  let cardText = document.createElement('div');
+  cardText.classList.add("card-text");
+  teamCard.append(cardText);
+  
+  //inserisco i dati nei div
   for (let key in utente){
     cardImage.innerHTML=`
     <img src="${utente.foto}"/>
